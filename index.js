545a02716192b1b563b2c54abd54fbf4b77cf1b4
Veselin <vesselin60@gmail.com>
2020-12-08 16:57:15 +0200

    283. The Header Componentç

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Import a library to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+import App from './App';
 
 // Create a component
-const App = () => {
-  return (
-    <Text>Some Text</Text>
-  );
-};
+//const App = () => {
+  //return (
+    //<Text>Some Text</Text>
+  //);
+//};
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
