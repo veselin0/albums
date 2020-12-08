ca1cd357886ceccfcf256859001c8dd8d3149d9b
Veselin <vesselin60@gmail.com>
2020-12-08 18:22:10 +0200

    286. More on Styling Components

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: 'gray',
+  },
   textStyle: {
     fontSize: 36,
   },
