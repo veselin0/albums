f77114861d2e2bbcde27a8ed9c3ac4896cef46f1
Veselin <vesselin60@gmail.com>
2020-12-08 16:12:53 +0200

    279. Creating a Component with JSX

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Import a library to help create a component
 import React from 'react';
 import ReactNative from 'react-native';
+import TextInputNativeCommands from 'react-native/Libraries/Components/TextInput/TextInputNativeCommands';
 
 // Create a component
-
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
 // Render it to the device
