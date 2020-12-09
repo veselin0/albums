ae89fa8de461d64401823987a089f1229c950789
Veselin <vesselin60@gmail.com>
2020-12-09 10:32:37 +0200

    Spirit Of Hale

---
+++ src/components/AlbumLIst.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Album List!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AlbumList;
