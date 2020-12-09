64760aec967f37303616cfbeb3ca39a53cdcf57f
Veselin <vesselin60@gmail.com>
2020-12-09 17:07:06 +0200

    300. Fantastic Reusable Components - The Card

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Card = () => {
+  return (
+    <View></View>
+  );
+};
+
+const style = StyleSheet.create({});
+
+export default Card;
