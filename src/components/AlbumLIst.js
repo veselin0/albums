bf62cf91a92689a501bed227987410444c36c409
Veselin <vesselin60@gmail.com>
2020-12-09 16:41:45 +0200

    298. Rendering a List of Components

--- src/components/AlbumLIst.js
+++ src/components/AlbumLIst.js
@@ -3,22 +3,25 @@ import { View, Text, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 export default class AlbumList extends Component {
-  state = {};
+  state = {
+    albums: [],
+  };
 
-  UNSAFE_componentWillMount() {
+  componentDidMount() {
     axios
       .get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then((response) => this.setState({ albums: response.data }));
+      .then((response) => this.setState({ albums: response.data }))
+      .catch(console.error);
+  }
+
+  renderAlbums() {
+    return this.state.albums.map((album) => (
+      <Text key={album.title}>{album.title}</Text>
+    ));
   }
 
   render() {
-    console.log(this.state);
-
-    return (
-      <View>
-        <Text>Album List!</Text>
-      </View>
-    );
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
