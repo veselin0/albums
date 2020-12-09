77a8b10df2c037dbfa5eca9b94f38112cc85917d
Veselin <vesselin60@gmail.com>
2020-12-09 15:25:31 +0200

    296. Component Level State

--- src/components/AlbumLIst.js
+++ src/components/AlbumLIst.js
@@ -3,13 +3,17 @@ import { View, Text, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 export default class AlbumList extends Component {
+  state = {};
+
   UNSAFE_componentWillMount() {
     axios
       .get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then((response) => console.log(response));
+      .then((response) => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>Album List!</Text>
