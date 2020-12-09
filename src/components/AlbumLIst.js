3b37b072c7adbed64e7667cb5979426cf99b87e4
Veselin <vesselin60@gmail.com>
2020-12-09 11:19:38 +0200

    293. Lifecycle Methods·

--- src/components/AlbumLIst.js
+++ src/components/AlbumLIst.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default class AlbumList extends Component {
+  UNSAFE_componentWillMount() {
+    console.log('Component will mount in AlbumList');
+  }
+
   render() {
     return (
       <View>
