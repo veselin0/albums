baa774dd2b9d1f7e50732c877f6e6fdc1e4be5a4
Veselin <vesselin60@gmail.com>
2020-12-10 16:11:46 +0200

    313. Linking Between Mobile Apps

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, StyleSheet, View, Image } from 'react-native';
+import { Text, StyleSheet, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
 
   return (
     <Card>
@@ -25,7 +25,7 @@ const AlbumDetail = ({ album }) => {
         <Image style={styles.imageStyle} source={{ uri: image }} />
       </CardSection>
       <CardSection>
-        <Button onPress={() => console.log(title)} />
+        <Button onPress={() => Linking.openURL(url)} />
       </CardSection>
     </Card>
   );
