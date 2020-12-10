0ab6715f92b12b6bbbbd9fc754f8e3d9b66f7fbe
Veselin <vesselin60@gmail.com>
2020-12-10 09:40:41 +0200

    306. More on Styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
-
 
 const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View></View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+});
 
 export default AlbumDetail;
--- src/components/Card.js
+++ src/components/Card.js
@@ -19,7 +19,7 @@ const style = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 2,
-    elevation: 1,
+    elevation: 5,
     marginLeft: 5,
     marginRight: 5,
     marginTop: 10,
