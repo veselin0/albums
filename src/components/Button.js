c00fbb798d810d8a6de8d9f1d89d70d9107e1c76
Veselin <vesselin60@gmail.com>
2020-12-10 15:19:32 +0200

    311. Styling of Buttons with UX Considerations

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity  } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Button = () => {
   return (
-    <TouchableOpacity>
-      <Text>Click me!!!</Text>
+    <TouchableOpacity style={styles.buttonStyle}>
+      <Text style={styles.textStyle}>Click me!!!</Text>
     </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+});
 
 export default Button;
